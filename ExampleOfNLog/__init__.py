ca14f1932268f79b16e22c03c9cc59c29f767195
WAKU-TAKE-A <waku-take-a@ymail.ne.jp>
2026-01-21 23:32:27 +0900

    update

--- ExampleOfNLog/__init__.py
+++ ExampleOfNLog/__init__.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """
-NLog for IronPython.
-
-* Environment variable 'IRONPYTHON_HOME' is required. It is the installation location of IronPython.
-* "NLog.dll" is required.
+NLog for IronPython (.NET 8.0)
 """
 
 __author__  = "WAKU-TAKE-A <waku-take-a@ymail.ne.jp>"
-__version__ = "0.9.1.0"
-__date__    = "2026/01/17"
+__version__ = "0.9.2.0"
+__date__    = "2026/01/21"
 
 #
 # append path.
@@ -17,28 +14,28 @@ __date__    = "2026/01/17"
 from pathlib import Path
 from sys import path as systemPath
 from System import Environment as env
+
+DOTNET_SHARED = sorted(Path(r"C:\Program Files\dotnet\shared\Microsoft.WindowsDesktop.App").glob("8.0.*"))[-1]
 IRONPYTHON_HOME = env.GetEnvironmentVariable("IRONPYTHON_HOME")
 
 if IRONPYTHON_HOME is None:
     raise Exception("Error : Set path of IRONPYTHON_HOME.")
 
 CURRENT_DIR = Path(__file__).resolve().parent
-
 IRONPYTHON_HOME_PATH = Path(IRONPYTHON_HOME)
 
-IRONPYTHON_LIB = IRONPYTHON_HOME_PATH / "Lib"
-IRONPYTHON_DLLS = IRONPYTHON_HOME_PATH / "DLLs"
-IRONPYTHON_NLOG = IRONPYTHON_HOME_PATH / "Lib" / "nlog"
-
-_lstPath = []
-_lstPath.append(IRONPYTHON_LIB)
-_lstPath.append(IRONPYTHON_DLLS)
-_lstPath.append(IRONPYTHON_NLOG)
+_lstPath = [
+    DOTNET_SHARED,
+    CURRENT_DIR,
+    IRONPYTHON_HOME_PATH / "Lib",
+    IRONPYTHON_HOME_PATH / "DLLs"
+]
 
 for i in _lstPath:
     if not i.exists():
-        raise FileNotFoundError("Required directory not found: {0}. Please check your IRONPYTHON_HOME path.".format(i))
-    systemPath.append(str(i))
+        raise FileNotFoundError("Required directory not found: {0}".format(i))
+    if str(i) not in systemPath:
+        systemPath.append(str(i))
 
 #
 # Import modules.
